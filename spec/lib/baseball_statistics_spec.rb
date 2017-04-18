48379d5a08d6db811bd1c38920ac88c050091409
John F. Mercer <john.f.mercer@gmail.com>
2017-04-18 14:07:27 -0400

    Add tests

---
+++ spec/lib/baseball_statistics_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+require "baseball_statistics"
+
+describe "BaseballStatistics" do
+  it "knows the truth" do
+    expect(true).to be(true)
+  end
+end
+
+describe BaseballStatistics do
+  it "loves baseball" do
+    expect("I love baseball").to eq("I love baseball")
+  end
+end
